a7055aed0f3062a13e665b567dd53d304005632e
unleashy <unleashy@users.noreply.github.com>
2024-10-19 19:21:28 -0300

    Enable prefetching

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,14 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: "hover",
+  },
   scopedStyleStrategy: "where",
   vite: {
     css: {
-      transformer: "lightningcss"
-    }
-  }
+      transformer: "lightningcss",
+    },
+  },
 });
