c484456b5a51601977abdcc674f81d23a9662bb6
unleashy <unleashy@users.noreply.github.com>
2024-10-19 00:31:12 -0300

    Start styling work

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,11 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  scopedStyleStrategy: "where",
+  vite: {
+    css: {
+      transformer: "lightningcss"
+    }
+  }
+});
